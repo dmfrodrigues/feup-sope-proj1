b7920babd50b92f5594707c97fd88941b4687fe0
Joao Sousa <joaosousa.pt@gmail.com>
2020-03-23 11:01:45 +0000

    Added functions to check if symbolic link / if directory

--- include/simpledu_stat.h
+++ include/simpledu_stat.h
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 /**
  * @brief Get size of file, in number of blocks.
@@ -13,5 +14,27 @@
  * @return off_t    Number of blocks if successful, -1 if failed
  */
 off_t simpledu_stat(const char *path, off_t blocksize);
+
+
+/**
+ * @brief Check if a file is a symbolic link or not.
+ * 
+ * @param path      Path of file
+ * @return true     If symbolic link
+ * @return false    Otherwise
+ */
+bool simpledu_symb_link(const char *path);
+
+/**
+ * @brief Check if a file is a directory or not.
+ * 
+ * @param path      Path of file
+ * @return true     If directory
+ * @return false    Otherwise
+ */
+bool simpledu_dir(const char *path);
+
+
+
 
 #endif //SIMPLEDU_STAT_H_INCLUDED
