02a809210031df49c77fd1a1190bf88b21ffad37
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-03-29 04:14:15 +0100

    Changes to simpledu_iterate
    On regular file:
    - Only displays value if max_depth and all arguments allow it
    
    On directory:
    -Started lining out fork and exec of process; need to handle open DIR file still
    -Subtracts one from max_depth (should be the only change to be done on the args of new process)

--- src/unnamed_functions.c
+++ src/unnamed_functions.c
@@ -1,8 +1,15 @@
 #include "unnamed_functions.h"
 #include "simpledu_stat.h"
+#include "simpledu_args.h"
+#include <sys/stat.h>
 
 
-int simpledu_iterate(const char *path, int *pipe_pid) {
+int simpledu_iterate(const char *path, int *pipe_pid, simpledu_args_t arg, char *envp[]) {
+
+    //Sum of all file sizes in directory iteration
+    //This number will be sent to father process' pipe
+    long long result = 0;
+
 
     // check if the path given is a directory
     if (!simpledu_dir(path)) return -1;
@@ -21,7 +28,7 @@ int simpledu_iterate(const char *path, int *pipe_pid) {
 
     // Iterating over the items of a directory
     while ( (dir_point = readdir(dir_to_iter)) != NULL) {
-        
+
         // if dir
         if (simpledu_dir(dir_point->d_name)) {
             // process it
@@ -31,6 +38,21 @@ int simpledu_iterate(const char *path, int *pipe_pid) {
                 int execle(const char *pathname, const char *arg, ...
                           (char *) NULL,         char *const envp[]);
             */
+
+            //Must deal with dir_to_iter being open before doing fork (dont know how yet)
+
+            int pid = fork();
+            if (pid > 0) { //parent
+
+            } else if (pid == 0) { //child
+                if (arg.max_depth > 0) --arg.max_depth;
+                char **new_argv = NULL;
+                if (simpledu_args_toargv(&arg, &new_argv)) simpledu_exit(EXIT_FAILURE);
+                if (execve(dir_point->d_name, new_argv, envp)) simpledu_exit(EXIT_FAILURE);
+
+            } else {
+                simpledu_exit(EXIT_FAILURE);
+            }
         }
 
         // if symbolic link
@@ -40,13 +62,22 @@ int simpledu_iterate(const char *path, int *pipe_pid) {
 
         // if regular file
         else if (simpledu_reg_file(dir_point->d_name)) {
-
             // display size and relative path/(name)
             // Placeholder code. Make it display full relative path from "starting" directory
             long long file_size = simpledu_file_size(dir_point->d_name);
-            printf("%lld\t%s\n", file_size,  dir_point->d_name);
+            result += file_size / arg.block_size;
+
+            //If it needs to display file size
+            if (arg.max_depth > 0 && arg.all){
+                printf("%lld\t%s\n", file_size,  dir_point->d_name);
+            }
         }
 
+    }
+
+    //After iterating over directory, will try to display results
+    if (arg.max_depth > 0){
+        printf("%lld\t%s\n", result,  path);
     }
     
 
