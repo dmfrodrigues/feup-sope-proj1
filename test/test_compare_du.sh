13ae87550361b39c4eea63ba814da54a6e6289b2
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-01 19:33:59 +0100

    added more tests

--- test/test_compare_du.sh
+++ test/test_compare_du.sh
@@ -1,5 +1,11 @@
 set -e
+
 test/compare_du.sh "-l -S test/test-folder"
 test/compare_du.sh "-l -S ."
 test/compare_du.sh "-l -S include"
 test/compare_du.sh "-l -S acutest"
+
+test/compare_du.sh "-l -a -S test/test-folder"
+test/compare_du.sh "-l -a -S ."
+test/compare_du.sh "-l -a -S include"
+test/compare_du.sh "-l -a -S acutest"
