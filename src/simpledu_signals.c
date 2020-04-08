fa62a0a3aeabc5e87532adb6b0c2c5b6bb2c0e05
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-04-08 17:50:39 +0100

    Cleanup

--- src/simpledu_signals.c
+++ src/simpledu_signals.c
@@ -1,38 +1,36 @@
 #define _XOPEN_SOURCE 700
 #include "simpledu_signals.h"
 
-#include "simpledu_args.h"
-#include "arg.h"
-
-#include <stdio.h> 
 #include <signal.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include "arg.h"
+#include "simpledu_args.h"
 
 static pid_t children_group;
-void initialize_data(pid_t p){
-    children_group = p;
-}
+void initialize_data(pid_t p) { children_group = p; }
 
-int simpledu_handler(){
+int simpledu_handler() {
     struct sigaction action;
     sigset_t sigmask;
     pid_t pgid = arg.children_process_group;
     // char temp[999];
     // sprintf(temp, "HANDLER INST %d/%d\n", getpgrp(), pgid);
     // write(1, temp, strlen(temp));
-    if (pgid == 0) { //First process, will handle second group
+    if (pgid == 0) {  // First process, will handle second group
         action.sa_handler = sigint_handler;
         sigemptyset(&action.sa_mask);
         action.sa_flags = 0;
-        if (sigaction(SIGINT,&action,NULL) == -1) {
+        if (sigaction(SIGINT, &action, NULL) == -1) {
             return EXIT_FAILURE;
         }
-    } 
-    else { //Process in second group, no special handling apart from blocking SIGINT
+    } else {  // Process in second group, no special handling apart from
+              // blocking SIGINT
         sigemptyset(&action.sa_mask);
         sigaddset(&sigmask, SIGINT);
-        if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1){
+        if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1) {
             return EXIT_FAILURE;
         }
     }
@@ -40,7 +38,7 @@ int simpledu_handler(){
     return EXIT_SUCCESS;
 }
 
-void sigint_handler(int signo){
+void sigint_handler(int signo) {
     pid_t pgid = children_group;
 
     char x[999];
@@ -52,16 +50,16 @@ void sigint_handler(int signo){
         return;
     }
 
-    if (signo == SIGINT){
+    if (signo == SIGINT) {
         printf("\tPGIDCHILD %d\n", pgid);
         killpg(pgid, SIGSTOP);
-        
-            printf("Are you sure you want to terminate the program? (y/n)\n");
-            if (scanf("%c", input) != 1) return;
-            if (*input == 'n' || *input == 'N'){
-                killpg(pgid, SIGCONT);
-            } else /*if (*input == 'y' || *input == 'Y')*/{
-                killpg(pgid, SIGTERM);
-            }
+
+        printf("Are you sure you want to terminate the program? (y/n)\n");
+        if (scanf("%c", input) != 1) return;
+        if (*input == 'n' || *input == 'N') {
+            killpg(pgid, SIGCONT);
+        } else /*if (*input == 'y' || *input == 'Y')*/ {
+            killpg(pgid, SIGTERM);
+        }
     }
 }
