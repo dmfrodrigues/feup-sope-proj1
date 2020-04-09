c84e74de829facacecb1dfa2450a1d800920868d
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-04-09 18:00:47 +0100

    changed testers

--- test/compare_simpledu_du.sh
+++ test/compare_simpledu_du.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# GREEN_INTENSIVE=$(tput bold)$(tput setaf 2)
-# RED_INTENSIVE=$(tput bold)$(tput setaf 1)
-# NC_INTENSIVE=$(tput bold)
-# NC=$(tput sgr0)
-
 GREEN_INTENSIVE='\033[1;32m'
 RED_INTENSIVE='\033[1;31m'
 NC_INTENSIVE='\033[1m' # No Color
@@ -16,7 +11,7 @@ SIMPLEDU_ERR=/tmp/simpledu.err
 s="${NC_INTENSIVE}Test '$1' with args '$2'...${NC}"
 printf %-96b "$s"
 
-$1 $2 2> $SIMPLEDU_ERR | sort -k2 > $SIMPLEDU_OUT
+$1 $2 > $SIMPLEDU_OUT 2> $SIMPLEDU_ERR
 simpledu_exit="${PIPESTATUS[0]}"
 
 mypath=$(dirname $0)
